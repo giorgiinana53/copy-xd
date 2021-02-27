e6c164d1e2c7a3b7526a7f17064e23b26e167092
giorgiinana53 <67916549+giorgiinana53@users.noreply.github.com>
2021-02-27 23:16:58 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("hello world");
+
+var date = new Date 
+console.log(date);
+
+var array = [
+     {
+        name: "giorgi",
+        age:15
+     },
+     {
+         name: "saba",
+         age:16
+     }
+];
+
+console.log(array[1].name)
+
+ PrintName("tornike",20,30,40);
+function PrintName(name, age, ...more) {
+    console.log(more)
+}
+
+function f1 () {
+    console.log("function1")
+}
+var f2 = function(){
+    console.log("function2")
+}
+
+f3 = () => {
+    console.log("function3")
+}
+ f1()
+ f2()
+ f3()
+
+ var george = 5;
+
+ if(5>3){
+     console.log("giorgi sulelia");
+ } else{
+     console.log("giorgi chkviania")
+ }
